f89490dd1b3b2748ad069cb79ffa20bae999342f
Atsu3 <mifutsua@gmail.com>
2024-06-25 09:28:44 +0900

    update

--- xiao_esp32c3/serial-test_rasp/serial-test.py
+++ xiao_esp32c3/serial-test_rasp/serial-test.py
@@ -1,6 +1,6 @@
 import serial
 if __name__ == '__main__':
-    ser =serial.Serial('/dev/ttyAMA0', 9600, timeout=1)
+    ser =serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
     while True:
         if ser.in_waiting > 0:
